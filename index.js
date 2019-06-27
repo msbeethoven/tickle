16d05ae7044e8d7f6ab7195de773ef456d44a9f7
core <seorisachs@gmail.com>
2019-06-26 20:16:31 -0400

    Wrote first test. Trouble shooting the document testability

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ const healthConditions = [
 
 ]
 
-function adjustedCost(age) {
-  let baseCost = 100;
+function ageValidator(age) {
   let minAge = 18;
   if (age < minAge) {
 
@@ -33,14 +32,8 @@ function adjustedCost(age) {
 
   } 
     let ageDifference = age - minAge
-
     let multiplier = Math.floor(ageDifference / 5);
-
     return multiplier;
-
-    
-  
-  
 
 }
 
@@ -52,8 +45,7 @@ function baseByTwenty(fiveYears){
 }
 
 function returnCostIncrease(healthCondition, newCost) {
-  let conditionPercent = healthConditions.filter(condition => condition.issue === healthCondition)
-
+  let conditionPercent = healthConditions.filter(condition => condition.issue === healthCondition);
   let costFactor = conditionPercent[0].costIncrease //the pure factor percentage 
   let conditionBasePercentage = costFactor * newCost;
   let estimatedQuote = newCost + conditionBasePercentage
@@ -65,57 +57,77 @@ function returnCostIncrease(healthCondition, newCost) {
 function femaleDiscount(newEstimatedQuote) {
   let femDiscount = 12;
   return newEstimatedQuote - femDiscount
-
 }
 
-//this is where it gets validated
-// const quoteCreator = (person) => {
-//   let client = {}; //object to create the client. Composed of name, age, gender, condition from
-//   let name = client.name;
-//   let age = client.age;
-//   let gender = client.gender;
-//   let condition = client.condition;
 
-//   let newBaseCost = returnCostIncrease( adjustedCost(age) );
+function quoteCreator (customer){
+  let age = customer.age
+  console.log(age)
+  let healthIssue = customer.issue
+  let fiveYrBlocks = ageValidator(age)
+  let newBaseQuote = baseByTwenty(fiveYrBlocks)
+  let baseCostAndConditions = returnCostIncrease(healthIssue, newBaseQuote)
+  return customer.gender === 'female' ? femaleDiscount(baseCostAndConditions) : baseCostAndConditions
+}
 
-//   return client.gender === 'female' ? femaleDiscount(newBaseCost) : newBaseCost;
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.getElementById('insurance-form').addEventListener('submit', submit)
+// })
 
+// function submit (event) {
+//   event.preventDefault()
+//   let form = document.getElementById('insurance-form')
+//   let newCustomer = {}
+//   newCustomer.name = document.getElementById('name').value
+//   newCustomer.age = document.getElementById('age').value
+//   newCustomer.gender = document.getElementById('gender').value
+//   newCustomer.issue = document.getElementById('health-condition').value
+//   let quote = quoteCreator(newCustomer)
+//   displayQuote(newCustomer, quote)
 // }
 
-function quoteCreater(person) {
-  let age = person.age;
-  let issue = person.issue;
-  // let newBaseCost = returnCostIncrease(issue,  adjustedCost(age) );
-let fiveYearBlock = adjustedCost(age);
-let newBaseCost = baseByTwenty(fiveYearBlock);
-let baseCostWithIssues = returnCostIncrease(issue, newBaseCost);
-  return person.gender === 'female' ? femaleDiscount(baseCostWithIssues) : baseCostWithIssues;
+// function displayQuote (newCustomer, quote) {
+//   let target = document.querySelector('#your-price')
+//   let h3 = document.createElement('h3')
+//   h3.innerHTML = 'Your Estimated Quote is:'
+//   let p = document.createElement('p')
+//   p.innerHTML = newCustomer.name + ' - ' + '$' + quote
+//   target.parentNode.insertBefore(h3, target)
+//   target.parentNode.insertBefore(p, target)
+// }
+
+// function quoteCreater(person) {
+//   let age = person.age;
+//   let issue = person.issue;
+//   // let newBaseCost = returnCostIncrease(issue,  ageValidator(age) );
+// let fiveYearBlock = ageValidator(age);
+// let newBaseCost = baseByTwenty(fiveYearBlock);
+// let baseCostWithIssues = returnCostIncrease(issue, newBaseCost);
+//   return person.gender === 'female' ? femaleDiscount(baseCostWithIssues) : baseCostWithIssues;
   
-}
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+
+//   let client = {}
+//   client.name = document.getElementById('name').value;
+//   client.age = document.getElementById('age').value;
+//   client.gender = document.getElementById("gender").value;
+//   client.issue = document.getElementById("health-condition").value;
+//   let quote = quoteCreater(client)
 
 
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  let client = {}
-  client.name = document.getElementById('name').value;
-  client.age = document.getElementById('age').value;
-  client.gender = document.getElementById("gender").value;
-  client.issue = document.getElementById("health-condition").value;
-  let quote = quoteCreater(client)
-
-
-  document.getElementById('insurance-form').addEventListener('submit', function quoteCreator(e) {
-    e.preventDefault();
+//   document.getElementById('insurance-form').addEventListener('submit', function quoteCreator(e) {
+//     e.preventDefault();
 
 
     
     
-    let priceDisplay = document.getElementById("your-price")
-    priceDisplay.innerHTML = 'Your Estimated Quote is: '+ quote;
+//     let priceDisplay = document.getElementById("your-price")
+//     priceDisplay.innerHTML = 'Your Estimated Quote is: '+ quote;
 
-  })
-})
+//   })
+// })
 
 
 
--- test/test.js
+++ test/test.js
@@ -1,5 +1,23 @@
-const assert = require('assert')
-const mocha = require('mocha')
+const assert = require('assert');
+//const jsdom = require('jsdom-global');
+const mocha = require('mocha');
 const describe = mocha.describe
 const it = mocha.it
-const program = require('../index')
+// global.document = jsdom();
+const program = require('../index');
+
+
+
+describe('insurance quote estimator', () => {
+  describe('ageValidator()', () => {
+    it('should return number', () => {
+      let age = 18
+      let expected = true
+      let actual = typeof program.ageValidator(age) === 'number'
+      assert(expected === actual)
+    })
+    
+    
+  })
+
+})
